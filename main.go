0bf4c1fa97b2f75607b6957ecc717a75b32b6e23
UmarIqbal124 <123955400+UmarIqbal124@users.noreply.github.com>
2024-02-05 21:49:45 +0500

    Update main.go

--- main.go
+++ main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Wellcome")
+	var num int
+	fmt.Print("Wellcome\n")
+	fmt.Print("Enter number: ")
+	fmt.Scan(&num)
+	fmt.Printf("You enter: %d ", num)
 }
